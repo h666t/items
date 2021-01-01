6c56a7a1358a819a242f1c8a4bc1a6006ce5e3f4
h666t <1845720928@qq.com>
2021-01-01 14:00:47 +0800

    add some item

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,16 +1,48 @@
-import {createStore} from 'redux'
+import {createStore} from 'redux';
 
 
-const reducer = (state: any,action: any) => {
-  state = state || { n : 0 }
-  switch (action.type) {
-    case 'add':
-      return {n:state.n + action.payload}
-    default :
-      return state
-  }
+type StateItem = {
+  name: string,
+  url: string,
+  github: string
 }
 
-const store = createStore(reducer)
+const reducer = (state: { [key: string]: StateItem }, action: any) => {
+  state = state || {
+    'block-ui': {
+      name: 'block-ui 组件库',
+      url: 'http://huang-haotian.gitee.io/block-uiwebsite/#/',
+      github: 'https://github.com/h666t/blockui'
+    },
+    'tally-react': {
+      name: '极简记账-react版',
+      url : "http://huang-haotian.gitee.io/tally-react/#/",
+      github : "https://github.com/h666t/tally-react"
+    },
+    'tally-vue' : {
+      name : "极简记账-vue版",
+      url : "http://huang-haotian.gitee.io/tally-vue/#/",
+      github : "https://github.com/h666t/tally"
+    },
+    "search" : {
+      name : "search 搜索",
+      url : "http://huang-haotian.gitee.io/search/",
+      github : "https://github.com/h666t/search"
+    },
+    "gastly" : {
+      name : "会动的代码",
+      url : "http://huang-haotian.gitee.io/gastly/",
+      github : "https://github.com/h666t/Gastly"
+    }
+  };
+  switch (action.type) {
+    case 'add':
+      return {n: state.n + action.payload};
+    default :
+      return state;
+  }
+};
 
-export {store}
+const store = createStore(reducer);
+
+export {store};
